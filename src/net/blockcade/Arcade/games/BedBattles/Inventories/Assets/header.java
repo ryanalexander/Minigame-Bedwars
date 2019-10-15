4fce7f6500d9a4cab245859ccd1c73e3c62a6443
Ryan W <w.ryan@stelch.com>
2019-10-15 13:47:21 +1100

    --- BedBattles --- Started menu overhaul (More details in next update)

--- src/net/blockcade/Arcade/games/BedBattles/Inventories/Assets/header.java
+++ src/net/blockcade/Arcade/games/BedBattles/Inventories/Assets/header.java
@@ -11,37 +11,12 @@
  *  @since 18/8/2019
  */
 
-/*
- *
- *  *
- *  * © Stelch Software 2019, distribution is strictly prohibited
- *  * Blockcade is a company of Stelch Software
- *  *
- *  * Changes to this file must be documented on push.
- *  * Unauthorised changes to this file are prohibited.
- *  *
- *  * @author Ryan Wood
- *  @since 18/8/2019
- */
-
-/*
- *
- *  *
- *  * © Stelch Software 2019, distribution is strictly prohibited
- *  * Blockcade is a company of Stelch Software
- *  *
- *  * Changes to this file must be documented on push.
- *  * Unauthorised changes to this file are prohibited.
- *  *
- *  * @author Ryan Wood
- *  @since 18/8/2019
- */
-
 package net.blockcade.Arcade.games.BedBattles.Inventories.Assets;
 
 import net.blockcade.Arcade.Game;
 import net.blockcade.Arcade.Utils.Item;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -49,7 +24,7 @@ import org.bukkit.inventory.ItemStack;
 public class header {
 
     public static Inventory format(Game game, Inventory inv, boolean header) {
-        ItemStack placeholder = new Item(Material.LIGHT_GRAY_STAINED_GLASS_PANE, "&r").setOnClick(new Item.click() {
+        ItemStack placeholder = new Item(Material.BLACK_STAINED_GLASS_PANE, "&r").setOnClick(new Item.click() {
             @Override
             public void run(Player param1Player) {
 
@@ -60,6 +35,15 @@ public class header {
         }
 
         if (header) {
+            Item quick_buy = new Item(Material.WHITE_STAINED_GLASS_PANE, "&fQuick Buy");
+            quick_buy.setOnClick(new Item.click() {
+                @Override
+                public void run(Player p) {
+                    p.playSound(p.getLocation(), Sound.ENTITY_VILLAGER_NO,0.5f,0.5f);
+                }
+            });
+            quick_buy.setLore(new String[]{"&7To set this &bQuick Buy&7 slot","&7just &bShift + Click&7 and click","&7any item from the menu"});
+
             Item Blocks = new Item(Material.END_STONE, "&6Blocks");
             Item Weapons = new Item(Material.STONE_SWORD, "&6Weapons");
             Item Armor = new Item(Material.IRON_CHESTPLATE, "&6Armor");
@@ -111,6 +95,17 @@ public class header {
             inv.setItem(14, Potions.spigot());
             inv.setItem(15, Eggs.spigot());
             inv.setItem(16, Special.spigot());
+
+            /*
+             Quick buy slots
+             */
+            inv.setItem(28,quick_buy.spigot());
+            inv.setItem(29,quick_buy.spigot());
+            inv.setItem(30,quick_buy.spigot());
+            inv.setItem(31,quick_buy.spigot());
+            inv.setItem(32,quick_buy.spigot());
+            inv.setItem(33,quick_buy.spigot());
+            inv.setItem(34,quick_buy.spigot());
         }
 
         return inv;
--- src/net/blockcade/Arcade/games/BedBattles/Inventories/shop.java
+++ src/net/blockcade/Arcade/games/BedBattles/Inventories/shop.java
@@ -11,32 +11,6 @@
  *  @since 18/8/2019
  */
 
-/*
- *
- *  *
- *  * © Stelch Software 2019, distribution is strictly prohibited
- *  * Blockcade is a company of Stelch Software
- *  *
- *  * Changes to this file must be documented on push.
- *  * Unauthorised changes to this file are prohibited.
- *  *
- *  * @author Ryan Wood
- *  @since 18/8/2019
- */
-
-/*
- *
- *  *
- *  * © Stelch Software 2019, distribution is strictly prohibited
- *  * Blockcade is a company of Stelch Software
- *  *
- *  * Changes to this file must be documented on push.
- *  * Unauthorised changes to this file are prohibited.
- *  *
- *  * @author Ryan Wood
- *  @since 18/8/2019
- */
-
 package net.blockcade.Arcade.games.BedBattles.Inventories;
 
 import net.blockcade.Arcade.Game;
